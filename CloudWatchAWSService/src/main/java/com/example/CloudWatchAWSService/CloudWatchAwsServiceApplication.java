772f9a687cb1b68e496f5bf09c14f399630ae7af
vivek-singh <viveksb1207@gmail.com>
2026-06-11 23:24:12 +0530

    initial commit

---
+++ CloudWatchAWSService/src/main/java/com/example/CloudWatchAWSService/CloudWatchAwsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.CloudWatchAWSService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWatchAwsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudWatchAwsServiceApplication.class, args);
+	}
+
+}
---
+++ CloudWatchAWSService/src/test/java/com/example/CloudWatchAWSService/CloudWatchAwsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CloudWatchAWSService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudWatchAwsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
